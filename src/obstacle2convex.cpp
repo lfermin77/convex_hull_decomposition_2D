948d2f1944cb867d336902c9f202b8687228f466
leonardo <leonardo@RedPill.(none)>
2016-06-07 13:03:03 +0200

    obstacle2convex

--- src/obstacle2convex.cpp
+++ src/obstacle2convex.cpp
@@ -1,6 +1,12 @@
+//ROS
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/Imu.h"
+
+#include "nav_msgs/GetMap.h"
+#include "nav_msgs/Odometry.h"
+#include "visualization_msgs/MarkerArray.h"
+
 
 
 //tf
@@ -8,8 +14,16 @@
 #include <tf/transform_listener.h>
 
 
+//openCV
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/imgproc/imgproc.hpp>
 
-class Imu2TF
+
+
+
+
+class obstacle2convex
 {
 	ros::NodeHandle n;
 	tf::StampedTransform transform;
@@ -17,20 +31,35 @@ class Imu2TF
 	static tf::TransformBroadcaster br_;
 
 	std::string imu_topic_;
+	ros::Subscriber map_sub_;
+	
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;	
+	cv_bridge::CvImagePtr cv_ptr;
+		
 
 
 
 
 	public:
-		Imu2TF(std::string& imu_topic) : imu_topic_(imu_topic)
+		obstacle2convex(std::string& imu_topic) : imu_topic_(imu_topic), it_(n)
 			{
 
 			ROS_INFO("Initializing class");
-			IMU_sub_ = n.subscribe(imu_topic_, 10, &Imu2TF::imuCallback, this);
+//			IMU_sub_ = n.subscribe(imu_topic_, 10, &obstacle2convex::imuCallback, this);
+			map_sub_ = n.subscribe("map", 1, &obstacle2convex::mapCallback, this);
+			
+			image_pub_ = it_.advertise("/image_frontier", 1);
+			
+			cv_ptr.reset (new cv_bridge::CvImage);
+			cv_ptr->encoding = "mono8";
+			
+			
 			ROS_INFO("After subscribe");
 		}
 		
-	~Imu2TF()
+	~obstacle2convex()
 		{
 		}
 		
@@ -59,6 +88,33 @@ class Imu2TF
 		}	
 		
 
+//////////////////////////////////		
+		void mapCallback(const nav_msgs::OccupancyGridConstPtr& map)
+		{
+			
+			cv::Mat grad;
+			
+			clock_t begin = clock();
+			
+			clock_t begin_process, end_process;
+			double elapsed_secs_process;// = double(end_process - begin_process) / CLOCKS_PER_SEC;
+//			end_process=clock();   elapsed_secs_process = double(end_process - begin_process) / CLOCKS_PER_SEC;			std::cerr<<"Time elapsed in process "<< elapsed_secs_process*1000 << " ms"<<std::endl;
+
+			{
+			std::cout <<"Map_Info_.resolution  " << map->info.resolution << std::endl;
+
+
+			ROS_INFO("Received a %d X %d map @ %.3f m/pix",
+				map->info.width,
+				map->info.height,
+				map->info.resolution);
+			 } 
+			 
+			 
+			cv_ptr->header = map->header;
+		}
+
+
 
 };
 
@@ -74,10 +130,10 @@ class Imu2TF
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "imu2tf");
+  ros::init(argc, argv, "obstacle2convex");
   std::string imu_topic = "imu/data";
     
-  Imu2TF converter(imu_topic);
+  obstacle2convex converter(imu_topic);
 
   ros::spin();
 
